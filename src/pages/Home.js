67467ecb3d20e2304efc418ce192d0e03521f9f5
Keyz400 <109141199+Keyz400@users.noreply.github.com>
2024-11-24 11:59:36 +0530

    Title replace to gif logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import "./Home.css";
+
 const Home = () => {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
@@ -10,7 +11,7 @@ const Home = () => {
   const scrollPosition = useRef(0);
   const location = useLocation();
   const navigate = useNavigate();
-  // Fetch movies function
+
   const fetchMovies = async () => {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/${categories}?api_key=40ea9ffe294065bfde6d35980bd08736&page=${page}`
@@ -18,13 +19,13 @@ const Home = () => {
     const data = await response.json();
     setMovies((prevMovies) => [...prevMovies, ...data.results]);
   };
-  // Handle category change
+
   const handleCategoryChange = (category) => {
     setCategories(category);
     setPage(1);
     setMovies([]);
   };
-  // Handle search input
+
   const handleSearch = async (e) => {
     setSearchQuery(e.target.value);
     if (e.target.value.length >= 3) {
@@ -37,26 +38,32 @@ const Home = () => {
       setSuggestions([]);
     }
   };
-  // Load more movies
+
   const loadMoreMovies = () => setPage((prevPage) => prevPage + 1);
-  // Save scroll position before navigation
+
   const saveScrollPosition = () => {
     scrollPosition.current = window.scrollY;
   };
-  // Restore scroll position on mount
+
   useEffect(() => {
     if (location.state?.scrollPosition) {
       window.scrollTo(0, location.state.scrollPosition);
     }
   }, [location.state]);
-  // Fetch movies when page or category changes
+
   useEffect(() => {
     fetchMovies();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [page, categories]);
+
   return (
     <div className="home-container">
-      <h1 className="site-title">VinJak Stream</h1>
+      <div className="site-logo">
+        <img
+          src={require("./output-onlinegiftools.gif")}
+          alt="VinJak Stream Logo"
+          className="gif-logo"
+        />
+      </div>
       <div className="category-buttons">
         <button
           onClick={() => handleCategoryChange("popular")}
@@ -135,4 +142,5 @@ const Home = () => {
     </div>
   );
 };
+
 export default Home;
